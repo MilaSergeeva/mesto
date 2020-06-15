6d5bbfdc2cfd43cfad4aa4bd69f6b8e13390fed8
Mila Sergeeva <buka4a@gmail.com>
2020-06-15 11:38:04 +0200

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,45 @@
+const popupOpenBtn = document.querySelector('.profile-info__popup-open');
+const popup = document.querySelector('.popup');
+const popupSaveBtn = popup.querySelector('.popup_btn-save');
+const popupCloseBtn = popup.querySelector('.popup__close');
+
+
+// Присваем модификатор для отрадения попап на странице
+const popupToggle = function (event) {
+    popup.classList.toggle('popup_opened');
+}
+
+const popupOverlayClose = function (event) {
+    if (event.target !== event.currentTarget) {return}
+    popupToggle()    
+}
+
+
+popupOpenBtn.addEventListener('click',popupToggle);
+popupCloseBtn.addEventListener('click',popupToggle);
+popup.addEventListener('click',popupOverlayClose);
+
+
+const userName = document.querySelector('.profile-info__name');
+const userOccupation = document.querySelector('.profile-info__occupation');
+const nameInput = document.querySelector('.popup__name');
+const occupationInput = document.querySelector('.popup__occupation');
+
+let saveElement = popup.querySelector('.popup_btn-save');
+function formSubmitHandler (event) {
+    event.preventDefault();
+    // nameInput.value = ' ';
+    // occupationInput.vslue = ' ';
+    userName.textContent = nameInput.value;
+    userOccupation.textContent = occupationInput.value;
+    
+}
+
+saveElement.addEventListener('submit', formSubmitHandler);
+
+// console.log ({
+//     popupOpenBtn,
+//     popup,
+//     popupSaveBtn,
+//     popupCloseBtn
+// });
