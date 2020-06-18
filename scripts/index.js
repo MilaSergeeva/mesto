c17afb59c505c32d2c57662b3a9936e20de448b4
Mila Sergeeva <buka4a@gmail.com>
2020-06-18 12:58:26 +0200

    second check

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,18 @@
-const popupOpenBtn = document.querySelector('.profile-info__popup-open');
+const popupOpenBtn = document.querySelector('.profile-info__edit-button');
 const popup = document.querySelector('.popup');
-const popupSaveBtn = popup.querySelector('.popup_btn-save');
+const popupSaveBtn = popup.querySelector('.popup__btn-save');
 const popupCloseBtn = popup.querySelector('.popup__close');
+const userName = document.querySelector('.profile-info__name');
+const userOccupation = document.querySelector('.profile-info__occupation');
+const nameInput = document.querySelector('input[name="user-name"]');
+const occupationInput = document.querySelector('input[name="user-occupation"]');
+const saveElement = popup.querySelector('.popup__form');
+
 
 const popupToggle = function (_event) {
     popup.classList.toggle('popup_opened');
+    nameInput.value = userName.textContent;
+    occupationInput.value = userOccupation.textContent;
 }
 
 const popupOverlayClose = function (event) {
@@ -15,19 +23,6 @@ const popupOverlayClose = function (event) {
     popupToggle();
 }
 
-popupOpenBtn.addEventListener('click', popupToggle);
-popupCloseBtn.addEventListener('click', popupToggle);
-popup.addEventListener('click', popupOverlayClose);
-
-const userName = document.querySelector('.profile-info__name');
-const userOccupation = document.querySelector('.profile-info__occupation');
-const nameInput = document.querySelector('.popup__name');
-const occupationInput = document.querySelector('.popup__occupation');
-
-nameInput.value = userName.textContent;
-occupationInput.value = userOccupation.textContent;
-
-const saveElement = popup.querySelector('.popup_btn-save');
 
 function formSubmitHandler(event) {
     event.preventDefault();
@@ -38,5 +33,9 @@ function formSubmitHandler(event) {
     popupToggle();
 }
 
-saveElement.addEventListener('click', formSubmitHandler);
+
+popupOpenBtn.addEventListener('click', popupToggle);
+popupCloseBtn.addEventListener('click', popupToggle);
+popup.addEventListener('click', popupOverlayClose);
+saveElement.addEventListener('submit', formSubmitHandler);
  
