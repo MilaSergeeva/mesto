4f6c96af300b956e0350629fff093a0f65bcc54c
Mila Sergeeva <buka4a@gmail.com>
2020-08-23 23:04:33 +0200

    forms UX

--- src/components /PopupWithForm.js
+++ src/components /PopupWithForm.js
@@ -1,12 +1,18 @@
 import { Popup } from './Popup.js';
 import { formSelector, formInputSelector } from '../utils/constants.js';
 
+// при нажатии на submit button
+//и до закрытия попап кнопка менялась
 class PopupWithForm extends Popup {
   constructor(popupSelector, handleFormSubmit) {
     super(popupSelector);
 
     this._formElement = this._element.querySelector(formSelector);
     this._handleFormSubmit = handleFormSubmit;
+    this._submitButton = this._formElement.querySelector('.popup__btn-save');
+
+    this._submitButtonDefaultLabel = this._submitButton.textContent.trim();
+    this._submitButtonSubmittingLabel = this._submitButton.dataset.submittingLabel;
   }
 
   _getInputValues() {
@@ -29,6 +35,7 @@ class PopupWithForm extends Popup {
     this._formElement.addEventListener('submit', (event) => {
       event.preventDefault();
 
+      this._submitButton.textContent = this._submitButtonSubmittingLabel;
       const formData = this._getInputValues();
 
       this._handleFormSubmit(formData);
@@ -37,7 +44,7 @@ class PopupWithForm extends Popup {
 
   closePopup() {
     super.closePopup();
-
+    this._submitButton.textContent = this._submitButtonDefaultLabel;
     this._formElement.reset();
   }
 }
