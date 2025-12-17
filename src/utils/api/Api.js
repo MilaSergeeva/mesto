c4daceafe6a4bd648d198e12462d5771df48dc94
Mila Sergeeva <buka4a@gmail.com>
2025-12-17 22:39:34 +0300

    webpack updated

--- src/utils/api/Api.js
+++ src/utils/api/Api.js
@@ -1,4 +1,4 @@
-import qs from 'querystring';
+// import qs from 'querystring';
 
 class Api {
   constructor({ baseUrl, headers }) {
@@ -11,30 +11,23 @@ class Api {
     let url = `${this.baseUrl}/${endpoint}`;
 
     const options = {
-      method: `${httpMethod}`,
+      method: httpMethod,
       headers: this.headers,
     };
 
     if (payload !== undefined && ['POST', 'PUT', 'PATCH'].includes(httpMethod)) {
       options.body = JSON.stringify(payload);
     } else if (payload !== undefined) {
-      const url = `${url}?${qs.encode(payload)}`;
+      url = `${url}?${new URLSearchParams(payload).toString()}`;
     }
 
     return fetch(url, options).then((res) => {
-      if (res.ok) {
-        // status code check
-        return res.json(); // process response
-      }
+      if (res.ok) return res.json();
 
-      // server respond with error 4xx
       if (res.status >= 400 && res.status < 500) {
-        return res.json().then((body) => {
-          return Promise.reject(`Что-то пошло не так: ${body.message}`);
-        });
+        return res.json().then((body) => Promise.reject(`Что-то пошло не так: ${body.message}`));
       }
 
-      // server didn't reply 5xx
       return Promise.reject(`Что-то пошло не так: ${res.status}`);
     });
   }
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const fs = require('fs');
 
 module.exports = {
   mode: 'development',
@@ -8,6 +9,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
+    publicPath: '',
   },
   module: {
     rules: [
@@ -18,7 +20,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        loader: [
+        use: [
           MiniCssExtractPlugin.loader,
           {
             loader: 'css-loader',
@@ -29,10 +31,10 @@ module.exports = {
           'postcss-loader',
         ],
       },
-      {
-        test: /\.html$/,
-        loader: 'html-loader',
-      },
+      // {
+      //   test: /\.html$/,
+      //   loader: 'html-loader',
+      // },
       {
         test: /\.(png|svg|jpg|gif|woff2|woff)$/,
         loader: 'file-loader',
@@ -41,7 +43,8 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      filename: 'index.html',
+      templateContent: fs.readFileSync(path.resolve(__dirname, './src/index.html'), 'utf-8'),
     }),
     new MiniCssExtractPlugin(),
   ],
